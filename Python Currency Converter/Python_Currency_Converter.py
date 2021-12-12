3f9b37afa0c922440c03ee8ea0857eea8dddbe28
Mustafa <mustafasaidhelal@gmail.com>
2021-12-12 21:18:07 +0100

    First Commit

---
+++ Python Currency Converter/Python_Currency_Converter.py
@@ -0,0 +1,12 @@
+while True:
+	print("SEK to USD Currency Converter")
+
+	print(" ")
+
+	sek = int(input("Enter your value in SEK to get USD Currency: "))
+
+	dollar = 0.11
+
+	print(f"{sek * dollar} USD")
+
+	print(" ")
